34dc60408d154e9ccb1881d3382c2d0ccfc47a8a
John Samuel <jsamchineme@gmail.com>
2018-11-14 16:14:51 +0100

    Implement JWT
    [finishes #161938843]

--- server/specs/parcels.js
+++ server/specs/parcels.js
@@ -1,4 +1,4 @@
-import { describe, it } from 'mocha';
+import { describe, it, before } from 'mocha';
 import { expect } from 'chai';
 import supertest from 'supertest';
 import app from '../app';
@@ -6,9 +6,23 @@ import app from '../app';
 const request = supertest(app);
 
 describe('Test case for the "parcel" resource endpoints', () => {
+  let authToken = '';
+  before((done) => {
+    request.post('/api/v1/auth/login')
+      .send({
+        email: 'jaden@example.io',
+        password: 'secret',
+      })
+      .expect(200)
+      .end((err, res) => {
+        authToken = res.body.token;
+        done();
+      });
+  });
+
   it('should get all parcels', (done) => {
     request.get('/api/v1/parcels')
-      .set('Content-Type', 'application/json')
+      .set('x-access-token', authToken)
       .expect(200)
       .end((err, res) => {
         expect(res.body.data.length > 0).to.equal(true);
@@ -26,6 +40,7 @@ describe('Test case for the "parcel" resource endpoints', () => {
       presentMapPointer: 'dummy value',
     };
     request.post('/api/v1/parcels')
+      .set('x-access-token', authToken)
       .send(parcelOrderData)
       .expect(200)
       .end((err, res) => {
@@ -37,6 +52,7 @@ describe('Test case for the "parcel" resource endpoints', () => {
   it('should get a specific parcel delivery order', (done) => {
     const parcelId = 1;
     request.get(`/api/v1/parcels/${parcelId}`)
+      .set('x-access-token', authToken)
       .expect(200)
       .end((err, res) => {
         expect(res.body.data.id !== undefined).to.equal(true);
@@ -44,8 +60,30 @@ describe('Test case for the "parcel" resource endpoints', () => {
         done();
       });
   });
+  it('should return failure when request does not have JWT token', (done) => {
+    const parcelId = 1;
+    request.get(`/api/v1/parcels/${parcelId}`)
+      .expect(200)
+      .end((err, res) => {
+        expect(res.body.status).to.equal('Failed');
+        expect(res.body.message).to.equal('missing token');
+        done();
+      });
+  });
+  it('should return failure when request has wrong JWT token', (done) => {
+    const parcelId = 1;
+    request.get(`/api/v1/parcels/${parcelId}`)
+      .set('x-access-token', 'xyz')
+      .expect(200)
+      .end((err, res) => {
+        expect(res.body.status).to.equal('Failed');
+        expect(res.body.message).to.equal('Authentication failed. Token could be expired or invalid');
+        done();
+      });
+  });
   it('should return message when record is not found for GET parcels/:id end point', (done) => {
     request.get('/api/v1/parcels/0')
+      .set('x-access-token', authToken)
       .expect(400)
       .end((err, res) => {
         expect(res.body.message).to.equal('NotFound');
@@ -54,6 +92,7 @@ describe('Test case for the "parcel" resource endpoints', () => {
   });
   it('should cancel a specific parcel delivery order', (done) => {
     request.put('/api/v1/parcels/1/cancel')
+      .set('x-access-token', authToken)
       .expect(200)
       .end((err, res) => {
         expect(res.body.message).to.equal('success');
@@ -63,6 +102,7 @@ describe('Test case for the "parcel" resource endpoints', () => {
   });
   it('should return message when record is not found for PUT parcels/:id/cancel end point', (done) => {
     request.put('/api/v1/parcels/0/cancel')
+      .set('x-access-token', authToken)
       .expect(400)
       .end((err, res) => {
         expect(res.body.message).to.equal('NotFound');
@@ -71,6 +111,7 @@ describe('Test case for the "parcel" resource endpoints', () => {
   });
   it('should update a specific parcel delivery order', (done) => {
     request.put('/api/v1/parcels/1')
+      .set('x-access-token', authToken)
       .send({ status: 'delivered' })
       .expect(200)
       .end((err, res) => {
@@ -81,6 +122,7 @@ describe('Test case for the "parcel" resource endpoints', () => {
   });
   it('should return message when record is not found for PUT parcels/:id end point', (done) => {
     request.put('/api/v1/parcels/0')
+      .set('x-access-token', authToken)
       .expect(400)
       .end((err, res) => {
         expect(res.body.message).to.equal('NotFound');
@@ -89,6 +131,7 @@ describe('Test case for the "parcel" resource endpoints', () => {
   });
   it('should return records for seleted user', (done) => {
     request.get('/api/v1/users/2/parcels')
+      .set('x-access-token', authToken)
       .expect(200)
       .end((err, res) => {
         expect(res.body.data.length > 0).to.equal(true);
--- server/v1/routes/user.js
+++ server/v1/routes/user.js
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 import ParcelController from '../controllers/ParcelController';
+import JWT from '../middlewares/JWT';
 
 const userRoutes = Router();
 
-userRoutes.get('/:userId/parcels', ParcelController.listForUser);
+userRoutes.get('/:userId/parcels', JWT.authenticate, ParcelController.listForUser);
 
 export default userRoutes;
