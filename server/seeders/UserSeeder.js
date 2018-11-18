e38309ad8c2d94690871cf0fd5f702fcbe9223f7
John Samuel <jsamchineme@gmail.com>
2018-11-19 00:20:31 +0100

    Update readme with collections and api endpoints
    [finishes #162039075]

--- server/seeders/UserSeeder.js
+++ server/seeders/UserSeeder.js
@@ -1,7 +1,9 @@
 import bcrypt from 'bcryptjs';
 import UserModel from '../models/User';
+import ParcelModel from '../models/Parcel';
 
 const User = new UserModel();
+const Parcel = new ParcelModel();
 
 /**
  * @class UserSeeder
@@ -35,6 +37,36 @@ class UserSeeder {
     const users = dummyUsers.map(async (item) => {
       const user = item;
       const newUser = await User.create(user);
+
+      const dummyParcels = [
+        {
+          userId: newUser.id,
+          description: 'description for delivered item',
+          status: 'delivered',
+          deliveryLocation: '5 pack street, Lagos',
+          presentLocation: '5 Victoria Land Street, Ikeja',
+          pickupLocation: '5 Victoria Land Street, Ikeja',
+          presentMapPointer: '45234232323',
+        },
+        {
+          userId: newUser.id,
+          description: 'some description of the item in the order',
+          status: 'pending_delivery',
+          deliveryLocation: '5 pack street, Lagos',
+          presentLocation: '5 Victoria Land Street, Ikeja',
+          pickupLocation: '5 Victoria Land Street, Ikeja',
+          presentMapPointer: '45234232323',
+        },
+      ];
+
+      const orders = dummyParcels.map(async (item) => {
+        const order = item;
+        const newOrder = await Parcel.create(order);
+        return newOrder;
+      });
+
+      const createdOrders = await Promise.all(orders);
+      newUser.orders = createdOrders;
       return newUser;
     });
 
